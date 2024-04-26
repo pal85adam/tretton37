85aff947fff0cd402843abd38ada66ac2d90726b
Mohammed Nijim <pal.85.adam@gmail.com>
2024-04-26 15:26:43 +0200

    feat: testing bad route to page not found

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,12 +1,25 @@
 // sum.test.js
 import { describe, expect, it } from 'vitest';
 import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
 
 import HomePage from './pages/HomePage';
+import App from './App';
 
 describe('App', () => {
   it('Home page contains HomePage', () => {
     render(<HomePage />);
     expect(screen.getByRole('paragraph')).toHaveTextContent('HomePage');
+  });
+});
+
+describe('Bad route', () => {
+  it('Page not found', () => {
+    render(
+      <MemoryRouter initialEntries={['/some/bad/route']}>
+        <App />
+      </MemoryRouter>,
+    );
+    expect(screen.getByText('NotFound')).toBeInTheDocument();
   });
 });
