dee5e7cc2044f14cc5eda04e0135fea4e8021568
Mohammed Nijim <pal.85.adam@gmail.com>
2024-05-03 09:15:28 +0200

    feat: config env file

--- src/configs.ts
+++ src/configs.ts
@@ -1,4 +1,3 @@
 export default {
-  apikey:
-    'api-key 14:2024-04-19:my.hiller@tretton37.com d98d47a5510a34b43a33e61722693fa9cb5e0f087397ad150c13981d6afc27d3',
+  apikey: import.meta.env.VITE_SOME_KEY,
 };
